fb367a1a5febad1273b39a06252dccaed7dcb668
imhanzala22 <71677063+imhanzala22@users.noreply.github.com>
2021-06-23 17:47:29 +0500

    navbar

--- sofa-house/src/App.js
+++ sofa-house/src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-import Home from './components/home';
-import Navbar from './components/Navbar';
+import home from './components/home';
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+
 
 function App() {
   return (
-    
     <Router>
-    <div className='app'>
-      <Switch>
-      <Route path="/Navbar" exact component={Navbar} />  
-      <Route path="/" exact component={Home} />  
-      </Switch>
-    </div>
-    </Router>
-    
-    
-    
+     
+    <Switch>
+      
+    <Route path="/" exact component={Navbar}  />  
+    <Route path="/" exact component={home}  />  
+   </Switch>
+  
+  </Router>
   );
 }
 
--- sofa-house/src/components/home.jsx
+++ sofa-house/src/components/home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+
 const Home = () => {
     return ( <div>
+       
         <h1>Greeting , <br /> Welcome to Sofa house</h1>
     </div> );
 }
