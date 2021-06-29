011d206a8da270e090db768f675bb07c9e6e24bb
imhanzala22 <71677063+imhanzala22@users.noreply.github.com>
2021-06-29 14:24:27 +0500

    carousel

--- sofa-house/src/App.js
+++ sofa-house/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import home from './components/home';
-import Navbar from './components/Navbar'
+import home from './components/home/home';
+import Navbar from './components/Navbar/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Carousel from './components/Carousel';
+import Footer from './components/Footer/Footer';
+import Carousle from './components/Carousel/Carousel';
+
 
 
 
@@ -12,11 +14,11 @@ function App() {
     <Router>
      <Navbar/>
     <Switch>
-    <Route path="/Carousel" component={Carousel} />
-    
+    <Route path="/Footer" exact component={Footer}  />  
+    <Route path="/Caroousel" exact component={Carousel}  />  
     <Route path="/" exact component={home}  />  
    </Switch>
-  
+    <Footer/>
   </Router>
   );
 }
